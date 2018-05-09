46712418de9dc5fb8268069dfd40ba19bb1d91b6
Abdel <achachia2003@yahoo.fr>
2018-05-09 11:30:03 +0200

    V1.0.6

--- gestion-admin/section_contenu.php
+++ gestion-admin/section_contenu.php
@@ -2,109 +2,153 @@
 <section>
     <!-- START Page content-->
     <section class="main-content">
-            <section class="main-content">
-        <div class="row">
-            <div class="col-lg-4">
-                <!-- START widget-->
-                <div class="panel widget">
-                    <div class="row row-table row-flush">
-                        <div class="col-xs-4 bg-info text-center"  style="min-height: 120px;">
-                            <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="panel-body text-center">
-                                <h4 class="mt0">FILMS</h4>
-                                <p class="mb0 text-muted"><?= $nbr_films; ?> films</p>
+        <section class="main-content">
+            <div class="row">
+                <div class="col-lg-4">
+                    <!-- START widget-->
+                    <div class="panel widget">
+                        <div class="row row-table row-flush">
+                            <div class="col-xs-4 bg-info text-center"  style="min-height: 120px;">
+                                <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="panel-body text-center">
+                                    <h4 class="mt0">FILMS</h4>
+                                    <p class="mb0 text-muted"><?= $nbr_films; ?> films</p>
 
+                                </div>
+                            </div>
+                            <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
+                                <h4 class="mt0">DATA</h4>
+                                <p class="mb0 text-muted"><?= $data_films; ?></p>
                             </div>
                         </div>
-                        <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
-                            <h4 class="mt0">DATA</h4>
-                            <p class="mb0 text-muted"><?= $data_films; ?></p>
-                        </div>
                     </div>
+                    <!-- END widget-->
                 </div>
-                <!-- END widget-->
-            </div>
-            <div class="col-lg-4">
-                <!-- START widget-->
-                <div class="panel widget">
-                    <div class="row row-table row-flush">
-                        <div class="col-xs-4 bg-danger text-center"  style="min-height: 120px;">
-                            <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="panel-body text-center">
-                                <h4 class="mt0">CARTOON</h4>
-                                <p class="mb0 text-muted"><?= $nbr_cartoon; ?> films</p>
+                <div class="col-lg-4">
+                    <!-- START widget-->
+                    <div class="panel widget">
+                        <div class="row row-table row-flush">
+                            <div class="col-xs-4 bg-danger text-center"  style="min-height: 120px;">
+                                <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="panel-body text-center">
+                                    <h4 class="mt0">CARTOON</h4>
+                                    <p class="mb0 text-muted"><?= $nbr_cartoon; ?> films</p>
 
+                                </div>
+                            </div>
+                            <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
+                                <h4 class="mt0">DATA</h4>
+                                <p class="mb0 text-muted"><?= $data_cartoon; ?></p>
                             </div>
                         </div>
-                        <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
-                            <h4 class="mt0">DATA</h4>
-                            <p class="mb0 text-muted"><?= $data_cartoon; ?></p>
-                        </div>
                     </div>
+                    <!-- END widget-->
                 </div>
-                <!-- END widget-->
-            </div>
 
-            <div class="col-lg-4">
-                <!-- START widget-->
-                <div class="panel widget">
-                    <div class="row row-table row-flush">
-                        <div class="col-xs-4 bg-success text-center"  style="min-height: 120px;">
-                            <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="panel-body text-center">
-                                <h4 class="mt0">SERIE TV</h4>
-                                <p class="mb0 text-muted"><?= $nbr_serie_tv; ?> serie</p>
+                <div class="col-lg-4">
+                    <!-- START widget-->
+                    <div class="panel widget">
+                        <div class="row row-table row-flush">
+                            <div class="col-xs-4 bg-success text-center"  style="min-height: 120px;">
+                                <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="panel-body text-center">
+                                    <h4 class="mt0">SERIE TV</h4>
+                                    <p class="mb0 text-muted"><?= $nbr_serie_tv; ?> serie</p>
 
+                                </div>
+                            </div>
+                            <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
+                                <h4 class="mt0">DATA</h4>
+                                <p class="mb0 text-muted"><?= $data_serie_tv; ?></p>
                             </div>
                         </div>
-                        <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
-                            <h4 class="mt0">DATA</h4>
-                            <p class="mb0 text-muted"><?= $data_serie_tv; ?></p>
-                        </div>
                     </div>
+                    <!-- END widget-->
                 </div>
-                <!-- END widget-->
+
+
             </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <!-- START widget-->
+                    <div class="panel widget">
+                        <div class="row row-table row-flush">
+                            <div class="col-xs-4 bg-info text-center"  style="min-height: 120px;">
+                                <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="panel-body text-center">
+                                    <h4 class="mt0">DOCUMENTAIRE FR</h4>
+                                    <p class="mb0 text-muted"><?= $nb_doc_fr; ?> documentaires</p>
 
-
-        </div>
-        <div class="row">
-            <div class="col-lg-4">
-                <!-- START widget-->
-                <div class="panel widget">
-                    <div class="row row-table row-flush">
-                        <div class="col-xs-4 bg-info text-center"  style="min-height: 120px;">
-                            <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
-                        </div>
-                        <div class="col-xs-6">
-                            <div class="panel-body text-center">
-                                <h4 class="mt0">DOCUMENTAIRE FR</h4>
-                                <p class="mb0 text-muted"><?= $nb_doc_fr; ?> documentaires</p>
-
+                                </div>
+                            </div>
+                            <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
+                                <h4 class="mt0">DATA</h4>
+                                <p class="mb0 text-muted"><?= $data_doc_fr; ?></p>
                             </div>
                         </div>
-                        <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
-                            <h4 class="mt0">DATA</h4>
-                            <p class="mb0 text-muted"><?= $data_doc_fr; ?></p>
+                    </div>
+                    <!-- END widget-->
+                </div>
+                <div class="col-lg-4">
+                    <!-- START widget-->
+                    <div class="panel widget">
+                        <div class="row row-table row-flush">
+                            <div class="col-xs-4 bg-info text-center"  style="min-height: 120px;">
+                                <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="panel-body text-center">
+                                    <h4 class="mt0">FILMS ARABIC</h4>
+                                    <p class="mb0 text-muted"><?= $nb_films_arabic; ?> films</p>
+
+                                </div>
+                            </div>
+                            <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
+                                <h4 class="mt0">DATA</h4>
+                                <p class="mb0 text-muted"><?= $data_films_arabic; ?></p>
+                            </div>
                         </div>
                     </div>
+                    <!-- END widget-->
                 </div>
-                <!-- END widget-->
+                <div class="col-lg-4">
+                    <!-- START widget-->
+                    <div class="panel widget">
+                        <div class="row row-table row-flush">
+                            <div class="col-xs-4 bg-info text-center"  style="min-height: 120px;">
+                                <em class="fa fa-film fa-2x"  style="padding-top: 40px;"></em>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="panel-body text-center">
+                                    <h4 class="mt0">FILMS HINDO</h4>
+                                    <p class="mb0 text-muted"><?= $nb_films_hindo; ?> films</p>
+
+                                </div>
+                            </div>
+                            <div class="col-xs-2 bg-info text-center"  style="min-height: 120px;padding-top: 20px">
+                                <h4 class="mt0">DATA</h4>
+                                <p class="mb0 text-muted"><?= $data_films_hindo; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END widget-->
+                </div>
+
             </div>
 
-        </div>    
 
-    </section>
-         
-             <?php 
-            
-              require dirname(__FILE__) . chemin_controleur . $module . "/" . $action . ".php";   ?>
+
+        </section>
+
+        <?php require dirname(__FILE__) . chemin_controleur . $module . "/" . $action . ".php"; ?>
 
         <!-- END panel-->
     </section>
