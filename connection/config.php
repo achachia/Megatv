722b2418e4a151e8d983f02da6939c1e5129457e
achachia <achachia2003@yahoo.fr>
2018-05-14 08:38:11 +0200

    Update config.php

--- connection/config.php
+++ connection/config.php
@@ -1,1 +1,50 @@
-﻿<?phpif ($_SERVER['SERVER_NAME'] == 'localhost') {    $dns = 'mysql:host=localhost;dbname=megatv_ip';    $user = 'root';    $password = '';    $host = 'http://localhost/MegatvProcedural';        $root_projet = 'C:\wamp64\www\MegacoursProcedural';} else {    $dns = 'mysql:host=localhost:3307;dbname=megatv_ip';    $user = 'achachia';    $password = '7130chachia';    $host = 'http://' . $_SERVER['SERVER_NAME'];        $root_projet = '/volume1/web/Megacours/MegacoursProcedural';}$url_espace_client=$host.'/espace_client';define('chemin_vue', '/vues/');define('chemin_modele', '/modele/');define('chemin_controleur', '/controleurs/');define('dir_media', 'http://megatv.fr/media');define('dir_vues', '/vues/');define('dir_modele', '/modele/');define('dir_controleur', '/controleurs/');define('root_web', getcwd());$options = array(    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);try {    $cxn = new PDO($dns, $user, $password, $options);} catch (Exception $e) {    echo "Connection à Mysql imposible : " . $e->getMessage();    die();}?>
+﻿<?php
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+
+    $dns = 'mysql:host=localhost;dbname=megatv_ip';
+
+    $user = 'root';
+
+    $password = '';
+
+    $host = 'http://localhost/MegatvProcedural';
+    
+    $root_projet = 'C:\wamp64\www\MegacoursProcedural';
+} else {
+
+   
+}
+
+$url_espace_client=$host.'/espace_client';
+
+define('chemin_vue', '/vues/');
+
+define('chemin_modele', '/modele/');
+
+define('chemin_controleur', '/controleurs/');
+
+define('dir_media', '');
+
+define('dir_vues', '/vues/');
+
+define('dir_modele', '/modele/');
+
+define('dir_controleur', '/controleurs/');
+
+define('root_web', getcwd());
+
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
+
+try {
+
+    $cxn = new PDO($dns, $user, $password, $options);
+} catch (Exception $e) {
+
+    echo "Connection à Mysql imposible : " . $e->getMessage();
+
+    die();
+}
+?>
