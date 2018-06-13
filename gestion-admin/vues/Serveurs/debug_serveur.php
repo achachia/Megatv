1387e71d313c5722904811915893752f9399f154
Abdel <achachia2003@yahoo.fr>
2018-06-13 09:11:42 +0200

    V1.0.8.8

--- gestion-admin/vues/Serveurs/debug_serveur.php
+++ gestion-admin/vues/Serveurs/debug_serveur.php
@@ -5,7 +5,21 @@
             <label class="control-label col-lg-3 col-md-3 col-sm-4" for="nom" style="color:blue;font-size:16px">IDENTIFIANT STREAMING: <span style="color:red">(<i class="fa fa-asterisk" aria-hidden="true"></i>)</span></label>
             <div class="col-sm-8">
 
-                <input type="text" class="form-control" id="identifiant_streaming"  name="identifiant_streaming"  placeholder="Entrer identifiant_streaming">
+                <input type="text" class="form-control" id="identifiant_streaming"  name="identifiant_streaming"  placeholder="Entrer identifiant_streaming"  value="<?php if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) { echo $_GET['identifiant_streaming']; }?>">
+
+                <?php if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) { ?>
+
+
+                    <input type="text" class="form-control" id="key_block_script"  name="key_block_script"  placeholder="Entrer le key block script"  value="<?php if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) { echo $_GET['key_block_script']; }?>">
+
+                <?php } ?>
+
+                <?php if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])  && isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) { ?>
+
+
+                    <input type="text" class="form-control" id="nbr_lettres"  name="nbr_lettres"  placeholder="Entrer le nombre de caracteres" value="<?php if (isset($_GET['nbr_lettres']) && !empty($_GET['nbr_lettres'])) { echo $_GET['nbr_lettres']; }?>">
+
+                <?php } ?>
 
                 <input type="hidden"  name="module"  value="Serveurs">
 
@@ -29,8 +43,8 @@
 
 
             $wikipediaURL = 'https://vidoza.net/embed-' . $_GET['identifiant_streaming'] . '.html';
-            
-          
+
+
 
 
 
@@ -52,16 +66,55 @@
             $wikipediaPage = new DOMDocument();
 //On y charge le contenu qu'on a récupéré avec cURL
             $wikipediaPage->loadHTML($resultat);
-            
-            var_dump($wikipediaPage->getElementsByTagName('script'));
 
 
 
             foreach ($wikipediaPage->getElementsByTagName('script') as $key => $script) {
 
-                echo $key;
-                var_dump($script->textContent);
-                echo '<br/>';
+                if (!isset($_GET['identifiant_streaming']) && empty($_GET['identifiant_streaming']) && empty($_GET['key_block_script'])) {
+
+                    echo $key;
+
+                    var_dump($script->textContent);
+
+                    echo '<br/>';
+                }
+
+
+
+                if ($key == $_GET['identifiant_streaming'] && !empty($_GET['identifiant_streaming'])) {
+
+
+
+                    $contenus = explode(",", $script->textContent);
+
+
+                    foreach ($contenus as $key1 => $contenu) {
+
+                        if (empty($_GET['nbr_lettres'])) {
+
+                            echo $key1;
+
+                            var_dump($contenu);
+
+                            echo '<br/>';
+                        }
+
+
+
+
+                        if ($key1 == $_GET['key_block_script']) {
+
+                            $chaine = substr($contenu, $_GET['nbr_lettres']);
+
+                            $chaine = substr($chaine, 0, -1);
+
+                            $lien1 = stripslashes($chaine);
+                            
+                            echo $lien1;
+                        }
+                    }
+                }
             }
         }
         ?>
