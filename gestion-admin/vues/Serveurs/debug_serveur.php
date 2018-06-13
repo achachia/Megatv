5a06715cf7012fc696a35f80849a93ac2965a188
Abdel <achachia2003@yahoo.fr>
2018-06-13 20:11:46 +0200

    V1.0.8.8

--- gestion-admin/vues/Serveurs/debug_serveur.php
+++ gestion-admin/vues/Serveurs/debug_serveur.php
@@ -1,19 +1,35 @@
 <div class="row" style="margin-top: 5%">
     <h2>Resulat de parsing :</h2>
-    
+
     <h2>
-        
-         <?php if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) { echo $_GET['key_block_script'].'-'; } ?>
-        
-         <?php if (isset($_GET['key_ligne_script']) && !empty($_GET['key_ligne_script'])) { echo $_GET['key_ligne_script'].'-'; } ?>
-        
-         <?php if (isset($_GET['nbr_lettres']) && !empty($_GET['nbr_lettres'])) { echo $_GET['nbr_lettres'].'-'; } ?>
-        
-        <?php if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) { echo $_GET['identifiant_streaming']; } ?>
-        
-         
-        
-        
+
+        <?php
+        if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) {
+            echo $_GET['key_block_script'] . '-';
+        }
+        ?>
+
+        <?php
+        if (isset($_GET['key_ligne_script']) && !empty($_GET['key_ligne_script'])) {
+            echo $_GET['key_ligne_script'] . '-';
+        }
+        ?>
+
+        <?php
+        if (isset($_GET['nbr_lettres']) && !empty($_GET['nbr_lettres'])) {
+            echo $_GET['nbr_lettres'] . '-';
+        }
+        ?>
+
+<?php
+if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) {
+    echo $_GET['identifiant_streaming'];
+}
+?>
+
+
+
+
     </h2>
 </div>
 
@@ -27,10 +43,10 @@
                 <div class="form-group"  style="padding-top:1%">
 
                     <input type="text" class="form-control" id="identifiant_streaming"  name="identifiant_streaming"  placeholder="Entrer identifiant_streaming"  value="<?php
-                    if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) {
-                        echo $_GET['identifiant_streaming'];
-                    }
-                    ?>">
+if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) {
+    echo $_GET['identifiant_streaming'];
+}
+?>">
 
                 </div>
 
@@ -38,7 +54,7 @@
         </div>
 
 
-        <?php if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) { ?>
+<?php if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) { ?>
 
             <div class="row" style="margin-top: 5%"  >
                 <div class="col-sm-4" style="text-align: center">
@@ -48,10 +64,10 @@
                     <div class="form-group"  style="padding-top:1%">
 
                         <input type="text" class="form-control" id="key_block_script"  name="key_block_script"  placeholder="Entrer le key block script"  value="<?php
-                        if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) {
-                            echo $_GET['key_block_script'];
-                        }
-                        ?>">
+    if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) {
+        echo $_GET['key_block_script'];
+    }
+    ?>">
 
                     </div>
 
@@ -59,9 +75,9 @@
             </div>
 
 
-        <?php } ?>
+<?php } ?>
 
-        <?php if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) { ?>
+<?php if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) { ?>
 
             <div class="row" style="margin-top: 5%">
                 <div class="col-sm-4"  style="text-align: center">
@@ -71,22 +87,22 @@
                     <div class="form-group"  style="padding-top:1%">
 
                         <input type="text" class="form-control" id="key_ligne_script"  name="key_ligne_script"  placeholder="Entrer le numero de ligne de script" value="<?php
-                        if (isset($_GET['key_ligne_script']) && !empty($_GET['key_ligne_script'])) {
-                            echo $_GET['key_ligne_script'];
-                        }
-                        ?>">
+    if (isset($_GET['key_ligne_script']) && !empty($_GET['key_ligne_script'])) {
+        echo $_GET['key_ligne_script'];
+    }
+    ?>">
 
 
                     </div>
 
                 </div>    
             </div>
-        <?php } ?>
+<?php } ?>
 
 
 
 
-        <?php if (isset($_GET['key_ligne_script']) && !empty($_GET['key_ligne_script'])) { ?>
+<?php if (isset($_GET['key_ligne_script']) && !empty($_GET['key_ligne_script'])) { ?>
 
             <div class="row" style="margin-top: 5%">
                 <div class="col-sm-4" style="text-align: center">
@@ -96,17 +112,17 @@
                     <div class="form-group"  style="padding-top:1%">
 
                         <input type="text" class="form-control" id="nbr_lettres"  name="nbr_lettres"  placeholder="Entrer le numero de ligne de script" value="<?php
-                        if (isset($_GET['nbr_lettres']) && !empty($_GET['nbr_lettres'])) {
-                            echo $_GET['nbr_lettres'];
-                        }
-                        ?>">
+    if (isset($_GET['nbr_lettres']) && !empty($_GET['nbr_lettres'])) {
+        echo $_GET['nbr_lettres'];
+    }
+    ?>">
 
 
                     </div>
 
                 </div>    
             </div>
-        <?php } ?>
+<?php } ?>
 
 
 
@@ -173,56 +189,45 @@
 
 
             if (strpos($script->textContent, '$( document ).ready(function()') !== FALSE) {
-                echo $key . '-test-<br/>';
-                
-                echo "toto";
-            }
 
-            if (empty($_GET['key_block_script'])) {
+                if (empty($_GET['key_block_script'])) {
 
-                echo $key . '<br/>';
+                    echo $key . '<br/>';
 
-                echo $script->textContent . '<br/>';
-            }
+                    echo $script->textContent . '<br/>';
+                    
+                } else {
 
+                    $contenus = explode(",", $script->textContent);
 
 
-            if ($key == $_GET['key_block_script']) {
+                    foreach ($contenus as $key1 => $contenu) {
+
+                        if (empty($_GET['key_ligne_script'])) {
 
 
+                            echo $key1;
+
+                            var_dump($contenu);
+
+                            echo '<br/>';
+                        }
 
 
-                $contenus = explode(",", $script->textContent);
+                        if ($key1 == $_GET['key_ligne_script']) {
 
+                            echo $contenu . "<br/>";
 
-                foreach ($contenus as $key1 => $contenu) {
+                            if (!empty($_GET['nbr_lettres'])) {
 
-                    if (empty($_GET['key_ligne_script'])) {
+                                $chaine = substr($contenu, $_GET['nbr_lettres']);
 
+                                $chaine = substr($chaine, 0, -1);
 
-                        echo $key1;
+                                $lien1 = stripslashes($chaine);
 
-                        var_dump($contenu);
-
-                        echo '<br/>';
-                    }
-
-
-
-
-                    if ($key1 == $_GET['key_ligne_script']) {
-
-                        echo $contenu . "<br/>";
-
-                        if (!empty($_GET['nbr_lettres'])) {
-
-                            $chaine = substr($contenu, $_GET['nbr_lettres']);
-
-                            $chaine = substr($chaine, 0, -1);
-
-                            $lien1 = stripslashes($chaine);
-
-                            echo $lien1;
+                                echo $lien1;
+                            }
                         }
                     }
                 }
