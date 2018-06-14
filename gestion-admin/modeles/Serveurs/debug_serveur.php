14a5923859710ce3b9cfa7ec537d32bfb2e6068a
Abdel <achachia2003@yahoo.fr>
2018-06-14 06:32:30 +0200

    V1.0.8.8

--- gestion-admin/modeles/Serveurs/debug_serveur.php
+++ gestion-admin/modeles/Serveurs/debug_serveur.php
@@ -1,74 +1,67 @@
 <?php
 
-function get_link_vidoza($identifiant, $param1 = NULL, $param2 = NULL, $nbr = NULL) {
+function get_link_vidoza($identifiant,$nbr_lettres) {
 
     //https://vidoza.net/embed-728qedpneqz1.html
 
     $wikipediaURL = 'https://vidoza.net/embed-' . $identifiant . '.html';
 
 
-
-//echo $wikipediaURL;
-//On initialise cURL
     $ch = curl_init();
-//On lui transmet la variable qui contient l'URL
+
     curl_setopt($ch, CURLOPT_URL, $wikipediaURL);
-//On lui demdande de nous retourner la page
+
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//On envoie un user-agent pour ne pas être considéré comme un bot malicieux
+
     curl_setopt($ch, CURLOPT_USERAGENT, 'Le blog de Samy Dindane (www.dinduks.com');
-//On exécute notre requête et met le résultat dans une variable
+
     $resultat = curl_exec($ch);
-//On ferme la connexion cURL
+
     curl_close($ch);
 
-//On crée un nouveau document DOMDocument
-    $wikipediaPage = new DOMDocument();
-//On y charge le contenu qu'on a récupéré avec cURL
-    $wikipediaPage->loadHTML($resultat);    
-   
 
+    $wikipediaPage = new DOMDocument();
+
+    $wikipediaPage->loadHTML($resultat);
+
+    $parsing = '';
+
+    $contenu_affiche = '';
 
     foreach ($wikipediaPage->getElementsByTagName('script') as $key => $script) {
 
-        echo $key;
-        var_dump($script->textContent);
-        echo '<br/>';
 
+        if (strpos($script->textContent, '$( document ).ready(function()') !== FALSE) {
 
-
-
-        if ($key == $param1) {
-
-
+            $parsing.=$key . '-';
 
             $contenus = explode(",", $script->textContent);
 
 
             foreach ($contenus as $key1 => $contenu) {
 
-                // echo $key1;
-                //var_dump($contenu);
-                //echo '<br/>';
+                if (strpos($contenu, 'window.player = player') !== FALSE) {
+
+                    $parsing.=$key1 . '-';
 
 
-                if ($key1 == $param2) {
 
-                    $chaine = substr($contenu, $nbr);
+                    $parsing.=$nbr_lettres . '-' . $identifiant;
+
+
+                    $chaine = substr($contenu, $nbr_lettres);
 
                     $chaine = substr($chaine, 0, -1);
 
                     $lien1 = stripslashes($chaine);
+
+
+                    $contenu_affiche.=$parsing . "</br/>" . $contenu . "<br/>" . $lien1;
+
+                    echo $contenu_affiche;
                 }
             }
         }
-    }
-
-
-
-    if (!is_null($nbr)) {
-
-        echo $lien1;
     }
 }
 
--- gestion-admin/vues/Serveurs/debug_serveur.php
+++ gestion-admin/vues/Serveurs/debug_serveur.php
@@ -1,41 +1,7 @@
-<div class="row" style="margin-top: 5%">
-    <h2>Resulat de parsing :</h2>
-
-    <h2>
-
-        <?php
-        if (isset($_GET['key_block_script']) && !empty($_GET['key_block_script'])) {
-            echo $_GET['key_block_script'] . '-';
-        }
-        ?>
-
-        <?php
-        if (isset($_GET['key_ligne_script']) && !empty($_GET['key_ligne_script'])) {
-            echo $_GET['key_ligne_script'] . '-';
-        }
-        ?>
-
-        <?php
-        if (isset($_GET['nbr_lettres']) && !empty($_GET['nbr_lettres'])) {
-            echo $_GET['nbr_lettres'] . '-';
-        }
-        ?>
-
-        <?php
-        if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) {
-            echo $_GET['identifiant_streaming'];
-        }
-        ?>
-
-
-
-
-    </h2>
-</div>
 
 <div class="row" style="margin-top: 5%">
     <form class="form-horizontal" id="debug_server" name="debug_server" method="GET" action="<?= $url_espace_admin; ?>/index.php?module=Serveurs&action=debug_serveur">
-        <div class="row" style="margin-top: 5%">
+        <div class="row" style="margin-top: 1%">
             <div class="col-sm-4"  style="text-align: center">
                 <label class="control-label" for="nom" style="color:blue;font-size:16px">IDENTIFIANT STREAMING: <span style="color:red">(<i class="fa fa-asterisk" aria-hidden="true"></i>)</span></label>
             </div>   
@@ -55,7 +21,7 @@
 
 
 
-        <div class="row" style="margin-top: 5%">
+        <div class="row" style="margin-top: 1%">
             <div class="col-sm-4" style="text-align: center">
                 <label class="control-label" for="nom" style="color:blue;font-size:16px">NOMBRE DE LETTRES: <span style="color:red">(<i class="fa fa-asterisk" aria-hidden="true"></i>)</span></label>
             </div>   
@@ -76,13 +42,11 @@
 
 
 
-
-
         <input type="hidden"  name="module"  value="Serveurs">
 
         <input type="hidden"  name="action"  value="debug_serveur">
 
-        <div class="row" style="margin-top: 5%">
+        <div class="row" style="margin-top: 2%">
 
             <div class="col-sm-12" style="text-align: center">
                 <div class="form-group">
@@ -102,72 +66,15 @@
 
 
 
-<div class="row" style="margin-top: 5%">
+<div class="row" style="margin-top: 3%">
 
     <?php
     if (isset($_GET['identifiant_streaming']) && !empty($_GET['identifiant_streaming'])) {
 
-        $wikipediaURL = 'https://vidoza.net/embed-' . $_GET['identifiant_streaming'] . '.html';
 
-
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $wikipediaURL);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Le blog de Samy Dindane (www.dinduks.com');
-
-        $resultat = curl_exec($ch);
-
-        curl_close($ch);
-
-
-        $wikipediaPage = new DOMDocument();
-
-        $wikipediaPage->loadHTML($resultat);
-
-        $parsing = '';
-
-        $contenu_affiche = '';
-
-        foreach ($wikipediaPage->getElementsByTagName('script') as $key => $script) {
-
-
-            if (strpos($script->textContent, '$( document ).ready(function()') !== FALSE) {
-
-                $parsing.=$key . '-';
-
-                $contenus = explode(",", $script->textContent);
-
-
-                foreach ($contenus as $key1 => $contenu) {
-
-                    if (strpos($contenu, 'window.player = player') !== FALSE) {
-
-                        $parsing.=$key1 . '-';
-
-
-
-                        $parsing.=$nbr_lettres . '-' . $_GET['identifiant_streaming'];
-
-
-                        $chaine = substr($contenu, $nbr_lettres);
-
-                        $chaine = substr($chaine, 0, -1);
-
-                        $lien1 = stripslashes($chaine);
-
-
-                         $contenu_affiche.=$parsing."</br/>".$contenu . "<br/>" . $lien1;
-                         
-                         echo $contenu_affiche;
-                    }
-                }
-            }
-        }
+        get_link_vidoza($_GET['identifiant_streaming'],$nbr_lettres);
     }
     ?>
 </div>
-</div>
+
 
